13ad9952edcc88c04c2650098983bfe50f3b30f3
richardjpurcell <richardjpurcell@yahoo.com>
2020-05-01 13:56:54 -0300

    Beginning files for project.

--- imageHSVsort/Source.cpp
+++ imageHSVsort/Source.cpp
@@ -1,7 +1,30 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
 
-int main() {
+using namespace cv;
+using namespace std;
 
+int main( int argc, char** argv) {
+
+	if (argc != 2)
+	{
+		cout << "Usage: display_image ImageToLoadAndDisplay" << endl;
+		return -1;
+	}
+
+	Mat image;
+	image = imread(argv[1], IMREAD_COLOR);
+
+	if (image.empty())
+	{
+		cout << "Could not open or find the image." << endl;
+		return -1;
+	}
+
+	imshow("Image", image);
+	int k = waitKey(0);
 
 	return 0;
 }
