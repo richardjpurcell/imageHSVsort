a38a891cbb8465e21a56e0319f0c7574aaef3edd
richardjpurcell <richardjpurcell@yahoo.com>
2020-05-02 21:44:37 -0300

    Working on fake histogram function.

--- imageHSVsort/Source.cpp
+++ imageHSVsort/Source.cpp
@@ -13,10 +13,12 @@ using namespace cv;
 using namespace std;
 
 int width, height;
-Mat image, imageHSV, hue, sat, val, outHue, outVal, outSat;
+Mat image, imageHSV, hue, sat, val, outHue, outVal, outSat, visualizeHue;
+vector<int> hueCount(256, 0);
 
 int insertionSort();
-int convertTo1D();
+void fakeHistorgram();
+
 
 int main( int argc, char** argv) {
 
@@ -34,8 +36,8 @@ int main( int argc, char** argv) {
 		return -1;
 	}
 
-	int width = image.cols;
-	int height = image.rows;
+	width = image.cols;
+	height = image.rows;
 	cvtColor(image, imageHSV, COLOR_BGR2HSV);
 	vector<Mat> hsvChannels;
 	split(imageHSV, hsvChannels);
@@ -45,6 +47,11 @@ int main( int argc, char** argv) {
 	val = hsvChannels[2].reshape(1, 1);
 
 	insertionSort();
+
+	for (int x : hueCount)
+		cout << x << " ";
+
+	cout << endl;
 
 	Mat out(image.rows, image.cols, CV_8UC3);
 
@@ -59,23 +66,34 @@ int main( int argc, char** argv) {
 
 	cvtColor(out, out, COLOR_HSV2BGR);
 
+	fakeHistorgram();
+
 	imshow("out", out);
 	imshow("Image", image);
+
 	int k = waitKey(0);
 
+	destroyAllWindows();
+
 	return 0;
+
 }
 
 
 int insertionSort() {
 	uchar keyHue, keySat, keyVal;
-
 	int  i, j;
+	int total = image.rows * image.cols;
+	int divider = total / 30;
+
+	cout << "|*";
+
 	for (i = 1; i < hue.cols; i++) {
 		keyHue = hue.at<uchar>(i);
 		keySat = sat.at<uchar>(i);
 		keyVal = val.at<uchar>(i);
 		j = i - 1;
+
 		while (j > 0 && hue.at<uchar>(j) > keyHue) {
 			hue.at<uchar>(j + 1) = hue.at<uchar>(j);
 			sat.at<uchar>(j + 1) = sat.at<uchar>(j);
@@ -85,6 +103,34 @@ int insertionSort() {
 		hue.at<uchar>(j + 1) = keyHue;
 		sat.at<uchar>(j + 1) = keySat;
 		val.at<uchar>(j + 1) = keyVal;
+
+		hueCount[keyHue] += 1;
+
+		if (i % divider == 0) {
+			cout << "*";
+		}
+
 	}
+	cout << "|" << endl;
+
 	return 0;
+}
+
+void fakeHistorgram() {
+	//make 2 vectors for each hue value (0-255) and 
+	//populate them with the sat or val value for that color
+	//then take the mean for that hue.
+	//step through a version of the image filling each pixel in color order
+	// with using the mean sat and val for that hue
+
+	visualizeHue = Mat::zeros(Size(180 * 4, 255 * 2), CV_8UC3);
+	cvtColor(visualizeHue, visualizeHue, COLOR_BGR2HSV);
+
+	for (int i = 0; i < 180; i++) {
+		int temp = (hueCount[i]/(float)(width*height)) * 255*20;
+
+		rectangle(visualizeHue, Point(i * 4, 255*2), Point(i * 4 + 4, 255*2 - temp), Scalar(i, 150, 150), -1);
+	}
+	cvtColor(visualizeHue, visualizeHue, COLOR_HSV2BGR);
+	imshow("visualize hue", visualizeHue);
 }
