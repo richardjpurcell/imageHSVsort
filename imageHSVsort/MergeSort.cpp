4d55d00891405dadd010bfbaca6fccf9e1a1654f
richardjpurcell <richardjpurcell@yahoo.com>
2020-05-16 13:54:02 -0300

    Rebuilding organization due to needs of merge sort.

--- imageHSVsort/MergeSort.cpp
+++ imageHSVsort/MergeSort.cpp
@@ -1,17 +1,69 @@
-/*
-* This program is used to test sorting functions on images
-* Author: Richard Purcell May 01 2020.
-* Notes: This follows the template inclusion model shown in
-*        https://docs.microsoft.com/en-us/cpp/cpp/source-code-organization-cpp-templates?view=vs-2019
-*/
+/*https://github.com/programmercave0/Algo-Data-Structure*/
+/*https://programmercave0.github.io/blog/2017/08/24/C++-Implementation-of-Merge-Sort*/
+
 
 #include <iostream>
 #include <vector>
-#include "sorting.h"
 
-int main( int argc, char** argv) {
+template<class T>
+void merge(std::vector<T>& v, int p, int q, int r)
+{
+    int size1 = q - p + 1;
+    int size2 = r - q;
+    std::vector<T> L(size1);
+    std::vector<T> R(size2);
 
+    for (int i = 0; i < size1; i++)
+    {
+        L[i] = v[p + i];
+    }
+    for (int j = 0; j < size2; j++)
+    {
+        R[j] = v[q + j + 1];
+    }
 
+    int i = 0, j = 0;
+    int k;
+    for (k = p; k <= r && i < size1 && j < size2; k++)
+    {
+        if (L[i] <= R[j])
+        {
+            v[k] = L[i];
+            i++;
+        }
+        else
+        {
+            v[k] = R[j];
+            j++;
+        }
+    }
+    for (i = i; i < size1; ++i)
+    {
+        v[k] = L[i];
+        k++;
+    }
+
+    for (j = j; j < size2; j++)
+    {
+        v[k] = R[j];
+        k++;
+    }
+}
+
+template<class T>
+void merge_sort(std::vector<T>& v, int p, int r)
+{
+    if (p < r)
+    {
+        int q = (p + r) / 2;
+        merge_sort(v, p, q);
+        merge_sort(v, q + 1, r);
+        merge(v, p, q, r);
+    }
+}
+
+int main()
+{
     std::vector<int>vec;
     vec.push_back(13);
     vec.push_back(5);
@@ -80,8 +132,4 @@ int main( int argc, char** argv) {
     std::cout << "\n";
 
     return 0;
-
-    return 0;
-
 }
-
---
+++ imageHSVsort/sorting.cpp
@@ -0,0 +1,63 @@
+
+/*https://github.com/programmercave0/Algo-Data-Structure*/
+/*https://programmercave0.github.io/blog/2017/08/24/C++-Implementation-of-Merge-Sort*/
+
+#include "sorting.h"
+
+template<class T>
+void merge(std::vector<T>& v, int p, int q, int r)
+{
+    int size1 = q - p + 1;
+    int size2 = r - q;
+    std::vector<T> L(size1);
+    std::vector<T> R(size2);
+
+    for (int i = 0; i < size1; i++)
+    {
+        L[i] = v[p + i];
+    }
+    for (int j = 0; j < size2; j++)
+    {
+        R[j] = v[q + j + 1];
+    }
+
+    int i = 0, j = 0;
+    int k;
+    for (k = p; k <= r && i < size1 && j < size2; k++)
+    {
+        if (L[i] <= R[j])
+        {
+            v[k] = L[i];
+            i++;
+        }
+        else
+        {
+            v[k] = R[j];
+            j++;
+        }
+    }
+    for (i = i; i < size1; ++i)
+    {
+        v[k] = L[i];
+        k++;
+    }
+
+    for (j = j; j < size2; j++)
+    {
+        v[k] = R[j];
+        k++;
+    }
+}
+
+template<class T>
+void merge_sort(std::vector<T>& v, int p, int r)
+{
+    if (p < r)
+    {
+        int q = (p + r) / 2;
+        merge_sort(v, p, q);
+        merge_sort(v, q + 1, r);
+        merge(v, p, q, r);
+    }
+}
+
