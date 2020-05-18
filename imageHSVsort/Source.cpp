d45b7b5ef4a1da0924628662fa851d786c125517
richardjpurcell <richardjpurcell@yahoo.com>
2020-05-18 16:18:30 -0300

    Merge-sort is working now.

--- imageHSVsort/Source.cpp
+++ imageHSVsort/Source.cpp
@@ -1,85 +1,58 @@
 /*
-* This program is used to test sorting functions on images
-* Author: Richard Purcell May 01 2020.
-* Notes: This follows the template inclusion model shown in
-*        https://docs.microsoft.com/en-us/cpp/cpp/source-code-organization-cpp-templates?view=vs-2019
+* Purpose: This program is used to test sorting functions on images
+* Author:  Richard Purcell May 01 2020.
+* Version: May 18, 2020.
+* Notes:   This follows the template inclusion model shown in
+*          https://docs.microsoft.com/en-us/cpp/cpp/source-code-organization-cpp-templates?view=vs-2019
 */
-
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
 #include <vector>
 #include "sorting.h"
 
+using namespace std;
+using namespace cv;
+
 int main( int argc, char** argv) {
 
+    string filename;
+    Mat img, imgGray;
+    vector<uchar>imgVec; //vector to hold 1d array
 
-    std::vector<int>vec;
-    vec.push_back(13);
-    vec.push_back(5);
-    vec.push_back(7);
-    vec.push_back(7);
-    vec.push_back(4);
-    vec.push_back(2);
-    vec.push_back(10);
-    int sz = vec.size();
-    std::cout << "Entered array : ";
-    for (int n = 0; n < sz; n++)
-    {
-        std::cout << vec[n] << " ";
+    if (argc != 2) {
+        cout << "usage:imageHSVsort.exe path_to_image" << endl;
+        cout << "ie:imageHSVsort.exe kodim04_patch.png" << endl << endl;
+        
+        filename = "./kodim01.png";
     }
-    std::cout << "\n";
-    std::cout << "Sorted array : ";
-    merge_sort(vec, 0, sz - 1);
-    for (int n = 0; n < sz; n++)
-    {
-        std::cout << vec[n] << " ";
+    else {
+        filename = argv[1];
     }
-    std::cout << "\n\n";
 
-    std::vector<char> c;
-    c.push_back('d');
-    c.push_back('y');
-    c.push_back('h');
-    c.push_back('l');
-    c.push_back('e');
-    c.push_back('a');
-    int sz1 = c.size();
-    std::cout << "Entered array : ";
-    for (int n = 0; n < sz1; n++)
-    {
-        std::cout << c[n] << " ";
-    }
-    std::cout << "\n";
-    std::cout << "Sorted array : ";
-    merge_sort(c, 0, sz1 - 1);
-    for (int n = 0; n < sz1; n++)
-    {
-        std::cout << c[n] << " ";
-    }
-    std::cout << "\n\n";
+    img = imread(filename);
 
-    std::vector<std::string> str;
-    str.push_back("car");
-    str.push_back("dog");
-    str.push_back("apple");
-    str.push_back("ball");
-    str.push_back("tree");
-    int sz2 = str.size();
+    cvtColor(img, imgGray, COLOR_BGR2GRAY);
 
-    std::cout << "Entered array : ";
-    for (int n = 0; n < sz2; n++)
-    {
-        std::cout << str[n] << " ";
+    //copy image to vector
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.cols; j++) {
+            imgVec.push_back(imgGray.at<uchar>(i, j));
+        }
     }
-    std::cout << "\n";
-    std::cout << "Sorted array : ";
-    merge_sort(str, 0, sz2 - 1);
-    for (int n = 0; n < sz2; n++)
-    {
-        std::cout << str[n] << " ";
-    }
-    std::cout << "\n";
 
-    return 0;
+    //sort by value
+    merge_sort(imgVec, 0, img.rows*img.cols);
+
+    //new image from vector with original dimensions
+    Mat imgSorted = Mat(imgVec).reshape(0,img.rows);
+
+    imshow("original image", img);
+    imshow("sorted image", imgSorted);
+    waitKey(0);
 
     return 0;
 
